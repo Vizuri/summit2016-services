657e94dd8cae242ebe958937804ab26eb3924959
cnourbakhsh <cnourbakhsh@vizuri.com>
2016-06-23 09:15:11 -0400

    Basic load of process image svg

--- mobile-claim-rest/src/main/webapp/js/app.js
+++ mobile-claim-rest/src/main/webapp/js/app.js
@@ -3,4 +3,8 @@
 
 	angular.module('bpmsFlowApp', [ 'bpmsFlowApp.controllers' ]);
 
+	angular.module('bpmsFlowApp').config([ '$httpProvider', function($httpProvider) {
+		$httpProvider.defaults.headers.common['Authorization'] = 'Basic Y25vdXJiYWtoc2g6cG90cGllMTIzPQ==';
+	} ]);
+
 })();
--- mobile-claim-rest/src/main/webapp/js/controllers.js
+++ mobile-claim-rest/src/main/webapp/js/controllers.js
@@ -5,11 +5,21 @@
 
 	angular.module('bpmsFlowApp.controllers').controller('MainController', mainController);
 
-	function mainController($log) {
+	function mainController($log, $http) {
 		$log.info('Inside MainController');
 		var vm = this;
 
 		vm.message = 'Up and running!';
+
+		$http({
+			method : 'GET',
+			withCredentials : true,
+			url : 'http://localhost:8080/business-central/rest/runtime/com.redhat.vizuri.insurance:mobile-claims-bpm:1.0-SNAPSHOT/process/mobile-claims-bpm.mobile-claim-process/image/193'
+		}).then(function(response) {
+			$log.info(response);
+		}, function(error) {
+			$log.error(error);
+		});
 
 	}
 
